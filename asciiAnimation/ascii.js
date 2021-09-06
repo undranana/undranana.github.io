707860a3ccc5613b802257f267addf9f23e2c5da
undranana <26426260+undranana@users.noreply.github.com>
2021-09-06 13:08:36 -0500

    update animation

--- asciiAnimation/ascii.js
+++ asciiAnimation/ascii.js
@@ -1,3 +1,7 @@
 function start(){
-	var i = setInterval(function(){},250)
+	var i = setInterval(function(){},250);
+	var mytextarea = document.getElementById("textarea");
+	var chosen = document.getElementById("animations");
+	console.log(chosen.options[chosen.selectedIndex]);
+	mytextarea.value=ANIMATIONS[chosen.options[chosen.selectedIndex].text];
 }
